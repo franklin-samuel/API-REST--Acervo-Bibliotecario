98c9a4d5e4d29129f86885a74e54d206a94f9fed
Pedrohenrc <pirriquecsousa@gmail.com>
2025-06-24 18:59:14 -0300

    Inicio do projeto - Criação de pastas

---
+++ models.py
@@ -0,0 +1,81 @@
+import uuid
+from datetime import datetime
+
+class BaseEntity:
+    def __init__(self):
+        self.id = uuid.uuid4()
+        self.data_criacao = datetime.now()
+
+    def __eq__(self, other):
+        return isinstance(other, self.__class__) and self.id == other.id
+
+    def _gerar_id(self):
+        return uuid.uuid4()
+    
+
+class obra(BaseEntity):
+    def __init__(self, titulo, autor, ano, categoria, quantidade=1):
+        super().__init__()
+        self.titulo = titulo
+        self.autor = autor
+        self.ano = ano
+        self.categoria = categoria
+        self.quantidade = quantidade
+    
+    def disponivel(self, estoque):
+        return estoque.get(self.id, 0) > 0
+    
+    def __str__(self):
+        return f"{self.titulo} ({self.ano})"
+
+class Usuario(BaseEntity):
+    def __init__(self, nome, email):
+        super().__init__()
+        self.nome = nome
+        self.email = email
+    
+    def __lt__(self, other):
+        return self.nome < other.nome
+    
+    def __str__(self):
+        return f"{self.nome}"
+
+
+class Emprestimo(BaseEntity):
+    def __init__(self, obra, usuario, data_emprestimo, data_prev_dev):
+        super().__init__()
+        self.obra = obra
+        self.usuario = usuario
+        self.data_emprestimo = data_emprestimo
+        self.previsao = data_prev_dev
+
+    def marcar_devolucao(self, data_devolucao):
+        self.data_devolucao = data_devolucao
+    
+    def dias_atraso(self, data_ref):
+        if data_ref > int(self.previsao):
+            return (data_ref - int(self.previsao)).days
+        else:
+            return 0
+        
+    def __str__(self):
+        return f"prev: {self.previsao}"
+
+class Acervo:
+    def __init__(self):
+        self.acervo = {} #dicionario: {Obra: qtd_disponivel}
+    
+    def __iadd__(self, obra):
+        if obra in self.acervo:
+            self.acervo[obra] += 1
+        else: 
+            self.acervo[obra] = obra
+        return Acervo
+
+    def __isub__(self, obra):
+        if obra in self.acervo:
+            if self.acervo[obra] > 1:
+                self.acervo[obra] -= 1
+            else:
+                self.acervo.remove(obra)
+    
