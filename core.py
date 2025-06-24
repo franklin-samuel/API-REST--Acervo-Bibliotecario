966813f309f15f27b90a059a75d9188b42ba16f8
Samuel Franklin <samuelfranklinff@gmail.com>
2025-06-24 20:26:14 -0300

    Update core.py

--- core.py
+++ core.py
@@ -1,5 +1,7 @@
 from models import BaseEntity, Usuario, obra, Emprestimo
 from datetime import datetime, timedelta
+from rich.table import Table, Console
+
 class Acervo:
     def __init__(self):
         self.acervo = {} #dicionario: {Obra: qtd_disponivel}
@@ -42,7 +44,29 @@ class Acervo:
 
     def valor_multa(self, emprestimo, data_ref):
         dias_atrasados = emprestimo.dias_atraso(data_ref)
-        user = Usuario
-        user.divida = f"R${dias_atrasados},00"
+        multa_por_dia = 1
+        valor_total = dias_atrasados * multa_por_dia
+
+        if valor_total > 0:
+            emprestimo.usuario.divida += valor_total
+
+    def relatorio_inventario(self):
+
+        console = Console()
+
+        table = Table(title="Todos os Livros", show_lines=True)
+        for col in ("id", "Título", "Autor", "Ano", "Categoria", "Quantidade"):
+            table.add_column(col, style="cyan", justify="center")
+        for obra, quantidade in self.acervo.items():
+            table.add_row(
+                str(obra.id),
+                obra.titulo,
+                obra.autor,
+                str(obra.ano),
+                obra.categoria,
+                str(obra.quantidade)
+            )
+
+        return table
         
     
